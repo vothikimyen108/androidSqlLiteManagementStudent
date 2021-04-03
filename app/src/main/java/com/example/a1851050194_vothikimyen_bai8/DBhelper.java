7725970f880a4d1d854c817161001e414aeb78ad
vothikimyen108 <vothikinyen2k@gmail.com>
2021-04-03 16:34:23 +0700

    update ui

---
+++ app/src/main/java/com/example/a1851050194_vothikimyen_bai8/DBhelper.java
@@ -0,0 +1,70 @@
+package com.example.a1851050194_vothikimyen_bai8;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBhelper extends SQLiteOpenHelper {
+
+
+    public DBhelper(Context context){
+        super(context,"userdate,db",null,1);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL( "create Table Userdetail(name Text primary key,contact Text,dob Text)" );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL( "drop Table if exists Userdetail" );
+    }
+    public boolean insterUserDate(String name, String contact, String dob){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put( "name",name );
+        contentValues.put("contact",contact);
+        contentValues.put("dob",dob);
+        long result = DB.insert( "Userdetail",null,contentValues );
+        if(result == -1)
+            return false;
+        else
+            return true;
+    }
+    public boolean updateUserDate(String name, String contact, String dob){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("contact",contact);
+        contentValues.put("dob",dob);
+        Cursor cursor = DB.rawQuery( "select * from Userdetail where name = ? " ,new String[]{name});
+        if(cursor.getCount()>=0){
+            long result = DB.update( "Userdetail",contentValues,"name=?",new String[]{name});
+            if(result == -1)
+                return false;
+            else
+                return true;
+        }else {
+            return  false;
+        }
+    }
+    public boolean deleteUserDate(String name){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery( "select * from Userdetail where name = ? " ,new String[]{name});
+        if(cursor.getCount()>=0){
+            long result = DB.delete( "Userdetail",null,new String[]{name});
+            if(result == -1)
+                return false;
+            else
+                return true;
+        }else {
+            return  false;
+        }
+    }
+    public Cursor getData(){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery( "select * from Userdetail " ,null);
+        return cursor;
+    }
+}
---
+++ app/src/main/java/com/example/a1851050194_vothikimyen_bai8/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.a1851050194_vothikimyen_bai8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText name, contact, dob;
+    Button insert, update, delete,view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+        anhXa();
+    }
+
+    private void anhXa() {
+        name = findViewById( R.id.editTextName );
+        contact = findViewById( R.id.editTextTextContact );
+        dob = findViewById( R.id.editTextBirthDay);
+        insert = findViewById( R.id.buttonInsert );
+        update = findViewById( R.id.buttonUpdate);
+        delete = findViewById( R.id.buttonDelete );
+    }
+}
